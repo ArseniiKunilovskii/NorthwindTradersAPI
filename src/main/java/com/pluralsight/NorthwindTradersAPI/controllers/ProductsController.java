621176599692f611265cf3a1d73337ca6fe8ca0d
arsenii <arseniy.kunilovskiy@gmail.com>
2025-12-09 14:39:37 -0500

    ProductsController is completed

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
@@ -0,0 +1,41 @@
+package com.pluralsight.NorthwindTradersAPI.controllers;
+
+
+import com.pluralsight.NorthwindTradersAPI.models.Product;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ProductsController {
+    private List<Product> products;
+
+    public ProductsController(){
+        products = new ArrayList<>();
+
+        products.add(new Product(1,"Pepsi",1,2.5));
+        products.add(new Product(1,"Coke",1,2.5));
+        products.add(new Product(1,"Red Wine",2,6));
+        products.add(new Product(1,"White Wine",2,6));
+    }
+
+    @RequestMapping(path = "/products", method = RequestMethod.GET)
+    public List<Product> getProducts(){
+        return products;
+    }
+
+    @RequestMapping(path = "/products/{productId}", method = RequestMethod.GET)
+    public Product getProductById(@PathVariable int productId){
+        for (Product product:products){
+            if(product.getProductId() == productId){
+                return product;
+            }
+        }
+        return null;
+    }
+
+}
